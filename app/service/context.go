b5424eb6b1bb91e1bd5827b0a6ca0b53459492f3
shaomengjie_win <954033026@qq.com>
2021-05-31 00:30:41 +0800

    注释规范

--- app/service/context.go
+++ app/service/context.go
@@ -9,10 +9,10 @@ import (
 //这里定义一个空的结构体，用来后面进行关联方法 方法即为某一结构体的可执行函数
 type contextService struct{}
 
-//一个空的结构体实例  作为service的上下文实例
+// Context 一个空的结构体实例  作为service的上下文实例
 var Context = contextService{}
 
-//此方法的作用是将model的上下文用model中key注册到request的上下文对象中 通过这种方法可以通过
+// Init 此方法的作用是将model的上下文用model中key注册到request的上下文对象中 通过这种方法可以通过
 //service实例Context拿到model对象
 func (s *contextService) Init(r *ghttp.Request, customCtx *model.Context) {
 	r.SetCtxVar(model.ContextKey, customCtx)
